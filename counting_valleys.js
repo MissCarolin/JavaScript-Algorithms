4e33a43ee057177ac3c2bc1901ffdd7be685e458
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-04-09 14:49:56 +0200

    solved algorithm revereString

--- counting_valleys.js
+++ counting_valleys.js
@@ -1,19 +1,15 @@
 /* PseudoCode
 Problem: find out how many Valleys Gary travelled through
-Define when he walkes a mountain (UD) and set that equal to 0 because we don't want to count that
-Define when he walks a valley (DU)
-Define, that the valley is only a valley if he walks as many step down as he walks up again, set tha equal to 1 to count his valleys
-Define, that for walking a valley he cant walk more steps up than down
+Define when he walkes walks up and count +1
+Define when he walks a down and count -1
+for walking a valley he cant walk more steps up than down
 count the valleys that match these steps criteria
-count downs and ups, if downs + ups equal and startet with D it means one valley = 1
 */
 
 function countingValleys(n, input) {
 const array = input.split('');
 console.log(array)
 
-// const ups = array.filter(i => i === 'U').length
-// const downs = array.filter(i => i === 'D').length
 
 let valleys = 0;
 let walk = 0;
---
+++ reverseString.js
@@ -0,0 +1,14 @@
+const reverseString = string => {
+  const array = string.split('').reverse().join('')
+}
+
+
+const reverseString2 = word => {
+  let reversedWord = '';
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word[i];
+  }
+    console.log(reversedWord)
+};
+reverseString2('codegod');
