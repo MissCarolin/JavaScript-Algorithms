604d168698b2be8e04c4a4f4ec26ee1a79a05c6e
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-04-15 15:11:02 +0200

    made changes to algorithm digitSum

--- digitSum.js
+++ digitSum.js
@@ -1,10 +1,24 @@
 const digitSum = str => {
-  const array = str.split(' ').map(Number).filter((number) =>  {
+  const nums = str.split(' ').map(Number).filter((number) =>  {
   return number;
   });
 
-  const total = array.reduce((a, b) => (a + b), 0);
+  const total = nums.reduce((a, b) => (a + b), 0);
   return total
 
 };
 console.log(digitSum('2 apples, 12 oranges'));
+
+/* ALTERNATIVE 2*/
+
+const digitSum2 = str => {
+  const nums = str.match(/\d+/g || []);
+  let sum = 0;
+
+  nums.forEach((num) => {
+      sum += Number(num)
+  });
+  return sum;
+}
+
+console.log(digitSum2('2 apples, 12 oranges'));
