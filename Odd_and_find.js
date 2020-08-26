eef6fc8bc12915a9c84f112c660bece0fb4d1d57
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-08-26 12:04:17 +0200

    added function

--- Odd_and_find.js
+++ Odd_and_find.js
@@ -1,11 +1,11 @@
-// const findNumber = (arr, k) => {
-//     if (arr.includes(k)) {
-//         return "YES";
-//     } else {
-//         return "NO";
-//     }
-// }
-// console.log(findNumber([1,2,3,4,5], 3));
+const findNumber = (arr, k) => {
+    if (arr.includes(k)) {
+        return "YES";
+    } else {
+        return "NO";
+    }
+}
+console.log(findNumber([1,2,3,4,5], 3));
 
 const oddNumbers = (l, r) => {
     const array = [];
